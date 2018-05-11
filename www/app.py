692d685c2167b468e0d0f1019665894360c88a13
Zack <389993194@qq.com>
2018-05-11 15:12:08 +0800

    Update framework Web and add config files

--- www/app.py
+++ www/app.py
@@ -10,16 +10,106 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment,FileSystemLoader
 
-from orm import Model,StringField,IntegerField
+import orm
+from coroweb import add_routes,add_static
+
+
+def init_jinja2(app,**kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape',True),
+		block_start_string = kw.get('block_start_string','{%'),
+		block_end_string = kw.get('block_end_string','%}'),
+		variable_start_string = kw.get('variable_start_string','{{'),
+		variable_end_string = kw.get('variable_end_string','}}'),
+		auto_reload = kw.get('auto_reload',True)
+	)
+	path = kw.get('path',None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'template')
+	logging.info('set jinja2 template path: %s' % path)
+	env = Environment(loader=FileSystemLoader(path),**options)
+	filters = kw.get('filters',None)
+	if filters is not None:
+		for name,f in filters.items():
+			env.filters[name] = f
+	app['__template__'] = env
+
+@asyncio.coroutine
+def logger_factory(app,handler):
+	@asyncio.coroutine
+	def logger(request):
+		logging.info('Request: %s %s' % (request.method,request.path))
+		return (yield from handler(request))
+	return logger
+
+@asyncio.coroutine
+def response_factory(app,handler):
+	@asyncio.coroutine
+	def response(request):
+		r = yield from handler(request)
+		if isinstance(r,web.StringResponse):
+			return r
+		if isinstance(r,bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r,str):
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if isinstance(r,dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Request(body=json.dumps(r,ensure_ascii=False,default=lambda o:o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Request(body=app['__template__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		if isinstance(r,int) and r>= 100 and r < 600:
+			return web.Response(r)
+		if isinstance(r,tuple) and len(r) == 2:
+			t,m = r
+			if isinstance(t,int) and t>= 100 and t < 600:
+				return web.Request(t,str(m))
+			resp = web.Request(body=str(r).encode('utf-8'))
+			resp.content_type = 'text/plain;charset=utf-8'
+			return resp
+		return response		
+
+
 
 def index(request):
 	return web.Response(body='<h1>Awesome</h1>',headers={'Content-Type':'text/html'})
 
+
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'1分钟前'
+	if delta < 3600:
+		return u'%s分钟前' % (delta // 60)
+	if delta < 86400:
+		return u'%s小时前' % (delta // 3600)
+	if delta < 604800:
+		return u'%s天前' % (delta // 86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year,dt.month,dt.day)
+
+
+
 @asyncio.coroutine
 def init(loop):
-	app = web.Application(loop=loop)
-	app.router.add_route('GET','/',index)
+	app = web.Application(loop=loop,middlewares=[logger_factory,response_factory])
+	init_jinja2(app,filters=dict(datetime=datetime_filter))
+	add_routes(app,'handlers')
+	add_static(app)
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	return srv
