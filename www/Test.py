bbd741bd06698b19cf82b1f06839ac463304de9e
Zack <389993194@qq.com>
2018-05-16 16:26:30 +0800

    Import css framework uikit and try a REST application

--- www/Test.py
+++ www/Test.py
@@ -9,7 +9,7 @@ loop = asyncio.get_event_loop()
 @asyncio.coroutine
 def test():
     yield from orm.create_pool(loop,user='root',password='1234',db='awesome')
-    user = User(name='Test',email='test@zack.com',passwd='1234',image='about:blank')
+    user = User(name='Zack',email='Zack@zack.com',passwd='1234',image='about:blank')
     yield from user.save()
 
 
--- www/handlers.py
+++ www/handlers.py
@@ -10,13 +10,26 @@ __author__ = 'Zack'
 from coroweb import get,post
 
 from models import User,Comment,Blog,next_id 
-import asyncio
+import asyncio,time
 
 @get('/')
-@asyncio.coroutine
 def index(request):
-	users = yield from User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1',name='Test Blog',summary=summary,created_at=time.time()-120),
+		Blog(id='2',name='Something New',summary=summary,created_at=time.time()-3600),
+		Blog(id='3',name='Learn Swift',summary=summary,created_at=time.time()-7200)
+	]
 	return {
-		'__template__': 'test.html',
-		'users': users
+		'__template__': 'blogs.html',
+		'blogs': blogs
 	}
+
+
+@get('/api/users')
+def api_get_users():
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
+
