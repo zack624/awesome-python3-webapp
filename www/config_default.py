72e5f43bbfbff14b73ac57239c03dd4293506d1a
Zack <389993194@qq.com>
2018-05-18 17:31:16 +0800

    Add register and login pages

--- www/config_default.py
+++ www/config_default.py
@@ -1,13 +1,13 @@
 configs = {
-	'debug': True,
-	'db': {
-		'host': '127.0.0.1',
-		'port': 3306,
-		'user': 'root',
-		'password': '1234',
-		'db': 'awesome'
-	},
-	'session': {
-		'secrect': 'awesome'
-	}
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '1234',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
 }
--- www/coroweb.py
+++ www/coroweb.py
@@ -92,51 +92,51 @@ class RequestHandler(object):
     def __call__(self, request):  # 直接将实例作为函数使用时，即调用了__call__方法
         kw = None
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
-        	if request.method == 'POST':
-        		if not request.content_type:
-        			return web.HTTPBadRequest('Missing Content-Type.')
-        		ct = request.content_type.lower()
-        		if ct.startswith('application/json'):
-        			params = yield from request.json()
-        			if not isinstance(params,dict):
-        				return web.HTTPBadRequest('JSON body must be object.')
-        			kw = params
-        		elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-        			params = yield from request.post()
-        			kw = dict(**params)
-        		else:
-        			return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
-        	if request.method == 'GET':
-        		qs = request.query_string
-        		if qs:
-        			kw = dict()
-        			for k, v in parse.parse_qs(qs,True).items():
-        				kw[k] = v[0]
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type.')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = yield from request.json()
+                    if not isinstance(params,dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = yield from request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs,True).items():
+                        kw[k] = v[0]
         if kw is None:
-        	kw = dict(**request.match_info)
+            kw = dict(**request.match_info)
         else:
-        	if not self._has_var_kw_arg and self._named_kw_args:
-        		copy = dict()
-        		for name in self._named_kw_args:
-        			if name in kw:
-        				copy[name] = kw[name]
-        		kw = copy
-        	for k, v in request.match_info.items():
-        		if k in kw:
-        			logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
-        		kw[k] = v
+            if not self._has_var_kw_arg and self._named_kw_args:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
         if self._has_request_arg:
-        	kw['request'] = request
+            kw['request'] = request
         if self._required_kw_args:
-        	for name in self._required_kw_args:
-        		if not name in kw:
-        			return web.HTTPBadRequest('Missing argument: %s' % name)
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
         logging.info('call with args: %s' % str(kw))
         try:
-        	r = yield from self._func(**kw)
-        	return r
+            r = yield from self._func(**kw)
+            return r
         except APIError as e:
-        	return dict(error=e.error,data=e.data,message=e.message)
+            return dict(error=e.error,data=e.data,message=e.message)
 
 
 def add_static(app):
